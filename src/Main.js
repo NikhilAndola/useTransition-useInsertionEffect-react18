d00e09a65567debc14ecdc2ac745a8f3f57c791b
NikhilAndola <nikhilandola123@gmail.com>
2022-06-07 12:51:50 +0530

    1 file modified, 1 added

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import UseTransition from './UseTransition';
+import { UseInsertion } from './UseInsertion';
 
 export default function Main() {
   return (
@@ -7,6 +8,7 @@ export default function Main() {
       <div>
         <h1>useTransition() and useInsertion() hook</h1>
 
+        <UseInsertion />
         <h2>useTransition()</h2>
         <UseTransition />
       </div>
---
+++ src/UseInsertion.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const UseInsertion = () => {
+  return (
+    <>
+      <h3>useInsertion() Definition:</h3>
+      <section>
+        <strong>
+          <code>useInsertionEffect(didUpdate);</code>
+        </strong>
+        <p>
+          The signature is identical to useEffect, but it fires synchronously
+          before all DOM mutations. Use this to inject styles into the DOM
+          before reading layout in useLayoutEffect. Since this hook is limited
+          in scope, this hook does not have access to refs and cannot schedule
+          updates.
+        </p>
+        <strong>Note: </strong>
+        <em>
+          useInsertionEffect should be limited to css-in-js library authors.
+          Prefer useEffect or useLayoutEffect instead.
+        </em>
+      </section>
+    </>
+  );
+};
